788cecf26a5a890d5cc02e95f71c922a2eab9c0b
Clinton Nguyen <clinton.ngyn@gmail.com>
2018-11-08 09:58:34 -0500

    Nav, link constants. Change index layout a bit

---
+++ src/constants/links.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    text: 'About',
+    to: '/about',
+  },
+  {
+    text: 'Tech',
+    to: '/tech',
+  },
+  {
+    text: 'Clothing',
+    to: '/clothing',
+  },
+  {
+    text: 'Ephemera',
+    to: '/ephemera',
+  },
+];
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import Header from '../components/Header';
+import PageLayout from '../components/PageLayout';
+import GalleryView from '../components/GalleryView';
 
 export default () => (
-  <Layout>
-    <Header title="Inventory" />
-    <div>Home / Apparel / Books / Ephemera</div>
-  </Layout>
+  <PageLayout>
+    <GalleryView />
+  </PageLayout>
 );
